7c56538a8494a7b17057279614f28f9d71ffc52a
caominhbao444 <minhbao11b3@gmail.com>
2024-03-05 21:34:21 +0700

    calendar

--- src/components/Event/CreateEvent.jsx
+++ src/components/Event/CreateEvent.jsx
@@ -2,25 +2,119 @@ import { useState } from "react";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { MultiInputDateTimeRangeField } from "@mui/x-date-pickers-pro/MultiInputDateTimeRangeField";
-import { Avatar, MenuItem, Select } from "@mui/material";
-
+import { Avatar, CircularProgress, MenuItem, Select } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import axios from "axios";
 const CreateEvent = () => {
-  const [method, setMethod] = useState(0);
+  const [method, setMethod] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [Urlimg, setUrlimg] = useState(
+    "https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-no-thumbnail-image-placeholder-for-forums-blogs-and-websites.jpg?ver=6"
+  );
+  const [imageUrl, setImageUrl] = useState(
+    "https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-no-thumbnail-image-placeholder-for-forums-blogs-and-websites.jpg?ver=6"
+  );
+  const [organizer, setOrganizer] = useState("Cao Minh Bao");
+  const [nameEvent, setNameEvent] = useState("");
+  const [dateEvent, setDateEvent] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [detail, setDetail] = useState("");
+  function handleFileChange(event) {
+    setLoading(true);
+    const selectedFile = event.target.files[0];
+    uploadImageToCloudinary(selectedFile)
+      .then((url) => {
+        setLoading(false);
+        setImageUrl(url);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  }
+  function uploadImageToCloudinary(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "pzoe2lzh"); // replace with your Cloudinary upload preset
+
+    return axios
+      .post("https://api.cloudinary.com/v1_1/djhhzmcps/image/upload", formData)
+      .then((response) => {
+        setUrlimg(response.data.url);
+        return response.data.url; // return the URL of the uploaded image
+      })
+      .catch((error) => {
+        console.error(error);
+        return null;
+      });
+  }
   const handleMethod = (e) => {
     setMethod(e.target.value);
+  };
+  const handleDateChange = (newvalue) => {
+    setDateEvent(newvalue);
+  };
+  const handleCreateEvent = (e) => {
+    e.preventDefault();
+    var dateStart = new Date(dateEvent[0].$d);
+    var dateEnd = new Date(dateEvent[1].$d);
+    const data = {
+      imageUrl: imageUrl,
+      dateEvent: dateEvent,
+      dateStart: dateStart,
+      dateEnd: dateEnd,
+      organizer: organizer,
+      nameEvent: nameEvent,
+      method: method,
+      amount: amount,
+      detail: detail,
+    };
+    console.log("Data is", data);
   };
   return (
     <div className="h-full w-full bg-white">
       <div className="w-full h-[200px] relative">
-        <img
-          src="https://images.unsplash.com/photo-1709380526836-100c551cfc28?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          className="w-full h-full object-cover object-center z-0"
-        />
-        <div className="flex absolute right-3 bottom-3 z-10 cursor-pointer gap-1 bg-white p-2 rounded-xl">
-          <AddPhotoAlternateIcon />
-          <span>Thêm ảnh</span>
+        {loading ? (
+          <>
+            <img
+              src={imageUrl}
+              width="100%"
+              alt="bag photos"
+              style={{
+                display: "block",
+                height: "200px",
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <CircularProgress />
+            </div>
+          </>
+        ) : (
+          <>
+            <img
+              src={imageUrl}
+              alt=""
+              className="w-full h-full object-cover object-center z-0"
+            />
+          </>
+        )}
+        <div className="flex absolute right-3 bottom-3 z-10 cursor-pointer gap-1 bg-white p-2 rounded-xl ">
+          <input
+            type="file"
+            className="opacity-0 flex absolute top-0 right-0 bottom-0 left-0 cursor-pointer rounded-xl z-10"
+            onChange={handleFileChange}
+          />
+          <AddPhotoAlternateIcon className="cursor-pointer" />
+          <span className="cursor-pointer">Thêm ảnh</span>
         </div>
       </div>
       <div className="w-full flex justify-start items-center px-4 py-2 border-b">
@@ -44,6 +138,8 @@ const CreateEvent = () => {
           Tên sự kiện
         </label>
         <input
+          value={nameEvent}
+          onChange={(e) => setNameEvent(e.target.value)}
           id="nameEvent"
           className="w-full outline-none border p-3"
           placeholder="Nhập tên sự kiện..."
@@ -53,6 +149,8 @@ const CreateEvent = () => {
         <h3>Thời gian diễn ra sự kiện</h3>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <MultiInputDateTimeRangeField
+            value={dateEvent}
+            onChange={handleDateChange}
             slotProps={{
               textField: ({ position }) => ({
                 label: position === "start" ? "Bắt đầu" : "Kết thúc",
@@ -67,8 +165,8 @@ const CreateEvent = () => {
           <MenuItem value="">
             <em>Vui lòng chọn hình thức</em>
           </MenuItem>
-          <MenuItem value={1}>Tổ chức Online</MenuItem>
-          <MenuItem value={2}>Tổ chức Offline</MenuItem>
+          <MenuItem value={"Online"}>Tổ chức Online</MenuItem>
+          <MenuItem value={"Offline"}>Tổ chức Offline</MenuItem>
         </Select>
       </div>
       <div className="w-full px-4 py-2 flex flex-col gap-3">
@@ -76,6 +174,8 @@ const CreateEvent = () => {
           Số lượng người tham gia
         </label>
         <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
           id="amount"
           className="w-full outline-none border p-3"
           placeholder="Nhập số lượng người tham gia"
@@ -87,6 +187,8 @@ const CreateEvent = () => {
           Mô tả sự kiện
         </label>
         <textarea
+          value={detail}
+          onChange={(e) => setDetail(e.target.value)}
           id="detail"
           className="w-full outline-none border p-3 resize-none h-[100px]"
           placeholder="Nhập số lượng người tham gia"
@@ -94,7 +196,10 @@ const CreateEvent = () => {
         />
       </div>
       <div className="w-full px-4 pt-2 pb-4 flex flex-col items-end gap-3">
-        <button className="px-4 py-2 rounded bg-mainColor text-white">
+        <button
+          className="px-4 py-2 rounded bg-mainColor text-white"
+          onClick={handleCreateEvent}
+        >
           Tạo sự kiện
         </button>
       </div>
--- src/components/Event/MyEvent.jsx
+++ src/components/Event/MyEvent.jsx
@@ -1,7 +1,320 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Menu,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import vi from "date-fns/locale/vi";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import ReorderIcon from "@mui/icons-material/Reorder";
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import { API_LISTEvents } from "../../fakeApi";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import WestIcon from "@mui/icons-material/West";
+import EastIcon from "@mui/icons-material/East";
+import {
+  format,
+  subHours,
+  startOfMonth,
+  startOfToday,
+  parse,
+  eachDayOfInterval,
+  endOfMonth,
+  add,
+  isSameDay,
+  parseISO,
+  isEqual,
+  isToday,
+  isSameMonth,
+  getDay,
+} from "date-fns";
+import {
+  MonthlyBody,
+  MonthlyDay,
+  MonthlyCalendar,
+  MonthlyNav,
+  DefaultMonthlyEventItem,
+} from "@zach.codes/react-calendar";
+const meetings = [
+  {
+    id: 1,
+    name: "Leslie Alexander",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2024-05-11T13:00",
+    endDatetime: "2024-05-11T14:30",
+  },
+  {
+    id: 2,
+    name: "Michael Foster",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2024-05-20T09:00",
+    endDatetime: "2024-05-20T11:30",
+  },
+  {
+    id: 3,
+    name: "Dries Vincent",
+    imageUrl:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2022-05-20T17:00",
+    endDatetime: "2022-05-20T18:30",
+  },
+  {
+    id: 4,
+    name: "Leslie Alexander",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2022-06-09T13:00",
+    endDatetime: "2022-06-09T14:30",
+  },
+  {
+    id: 5,
+    name: "Michael Foster",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2022-05-13T14:00",
+    endDatetime: "2022-05-13T14:30",
+  },
+  {
+    id: 6,
+    name: "Michael Foster",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2024-03-6T14:00",
+    endDatetime: "2024-05-13T14:30",
+  },
+  {
+    id: 7,
+    name: "Leslie Alexander",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    startDatetime: "2024-05-11T13:00",
+    endDatetime: "2024-05-11T14:30",
+  },
+];
+const Meeting = ({ meeting }) => {
+  let startDateTime = parseISO(meeting.startDatetime);
+  let endDateTime = parseISO(meeting.endDatetime);
+
+  return (
+    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+      <img
+        src={meeting.imageUrl}
+        alt=""
+        className="flex-none w-10 h-10 rounded-full"
+      />
+      <div className="flex-auto">
+        <p className="text-gray-900">{meeting.name}</p>
+        <p className="mt-0.5">
+          <time dateTime={meeting.startDatetime}>
+            {format(startDateTime, "h:mm a")}
+          </time>{" "}
+          -{" "}
+          <time dateTime={meeting.endDatetime}>
+            {format(endDateTime, "h:mm a")}
+          </time>
+        </p>
+      </div>
+      <div className="flex items-center">
+        <button className="px-3 py-1 bg-red-700 text-white cursor-pointer rounded-[12px]">
+          Hủy
+        </button>
+      </div>
+    </li>
+  );
+};
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+const FirstView = (props) => {
+  return (
+    <>
+      {API_LISTEvents.map((event) => {
+        return (
+          <div className="border" key={event.id}>
+            <img
+              src={event.image}
+              alt=""
+              className="w-full h-[200px] object-cover object-center"
+            />
+            <div className="flex flex-col w-full p-4 gap-4">
+              <h3 className="text-[16px] font-bold">{event.name}</h3>
+              <p>Người tổ chức: {event.organizer}</p>
+              <div className="flex flex-col md:flex-row w-full md:items-center">
+                <p className="w-full md:w-2/3 flex justify-start items-center">
+                  Thời gian: {event.date_start} - {event.date_end}
+                </p>
+                <p className="w-full md:w-1/3 flex justify-end items-center">
+                  Số lượng: {event.guest_current}/{event.guest_limit}
+                </p>
+              </div>
+              <div className="w-full">
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                  >
+                    Chi tiết
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <p>Hình thức tổ chức : {event.event_method}</p>
+                    <p>Nội dung: {event.desc}</p>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+              <div className="w-full flex justify-end items-center gap-3">
+                <button className="px-3 py-2 bg-red-700 text-white hover:bg-white hover:text-red-700 border border-red-700 rounded-lg">
+                  Huỷ tham gia
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+const SecondView = (props) => {
+  let today = startOfToday();
+  let [selectedDay, setSelectedDay] = useState(today);
+  let [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
+  let firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
+
+  let days = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfMonth(firstDayCurrentMonth),
+  });
+
+  function previousMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+  }
+
+  function nextMonth() {
+    let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+  }
+
+  let selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  );
+  return (
+    <>
+      <div className="pt-16">
+        <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+          <div className="md:grid md:grid-cols-1 md:divide-x md:divide-gray-200">
+            <div className="md:pr-14">
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-gray-900 uppercase">
+                  {format(firstDayCurrentMonth, "MMMM yyyy", { locale: vi })}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Previous month</span>
+                  <WestIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <EastIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+                <div>CN</div>
+                <div>Hai</div>
+                <div>Ba</div>
+                <div>Tư</div>
+                <div>Năm</div>
+                <div>Sáu</div>
+                <div>Bảy</div>
+              </div>
+              <div className="grid grid-cols-7 mt-2 text-sm">
+                {days.map((day, dayIdx) => (
+                  <div
+                    key={day.toString()}
+                    className={classNames(
+                      dayIdx === 0 && colStartClasses[getDay(day)],
+                      "py-1.5"
+                    )}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => setSelectedDay(day)}
+                      className={classNames(
+                        isEqual(day, selectedDay) && "text-white",
+                        !isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "text-red-500",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-900",
+                        !isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          !isSameMonth(day, firstDayCurrentMonth) &&
+                          "text-gray-400",
+                        isEqual(day, selectedDay) &&
+                          isToday(day) &&
+                          "bg-red-500",
+                        isEqual(day, selectedDay) &&
+                          !isToday(day) &&
+                          "bg-gray-900",
+                        !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                        (isEqual(day, selectedDay) || isToday(day)) &&
+                          "font-semibold",
+                        "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
+                      )}
+                    >
+                      <time dateTime={format(day, "yyyy-MM-dd")}>
+                        {format(day, "d")}
+                      </time>
+                    </button>
+
+                    <div className="w-1 h-1 mx-auto mt-1">
+                      {meetings.some((meeting) =>
+                        isSameDay(parseISO(meeting.startDatetime), day)
+                      ) && (
+                        <div className="w-1 h-1 rounded-full bg-sky-500"></div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <section className="mt-12 md:mt-0 md:pl-14">
+              <h2 className="font-semibold text-gray-900">
+                Schedule for{" "}
+                <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+                  {format(selectedDay, "MMM dd, yyy")}
+                </time>
+              </h2>
+              <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+                {selectedDayMeetings.length > 0 ? (
+                  selectedDayMeetings.map((meeting) => (
+                    <Meeting meeting={meeting} key={meeting.id} />
+                  ))
+                ) : (
+                  <p>No meetings for today.</p>
+                )}
+              </ol>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const MyEvent = () => {
   const [view, setView] = useState("left");
   const handleView = (event, newAlignment) => {
@@ -9,7 +322,7 @@ const MyEvent = () => {
   };
   return (
     <div className="h-full w-full bg-white flex flex-col gap-4 md:p-4">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center md:p-0 p-4">
         <h3 className="text-[18px] font-semibold">Danh sách sự kiện của tôi</h3>
         <div className="flex gap-3">
           <ToggleButtonGroup
@@ -27,9 +340,17 @@ const MyEvent = () => {
           </ToggleButtonGroup>
         </div>
       </div>
-      <div className="bg-red-400 h-full"></div>
+      {view === "left" ? <FirstView /> : <SecondView />}
     </div>
   );
 };
-
+let colStartClasses = [
+  "",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+];
 export default MyEvent;
